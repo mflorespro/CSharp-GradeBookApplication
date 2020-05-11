909d55065178b5cc854d8b31dd86837c0eee3912
mflorespro <marcelo.flores.mail@gmail.com>
2020-05-11 00:13:34 -0700

    Update RankedGradeBook.cs
    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,35 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (((this.Students.FindAll(x => x.AverageGrade >= 90).Count * 100) / this.Students.Count) <= 20)
+            {
+                return 'A';
+            }
+            else if ((((this.Students.FindAll(x => x.AverageGrade >= 80).Count * 100) / this.Students.Count) > 20) &&
+                    (((this.Students.FindAll(x => x.AverageGrade >= 80).Count * 100) / this.Students.Count) <= 40))
+            {
+                return 'B';
+            }
+            else if ((((this.Students.FindAll(x => x.AverageGrade >= 80).Count * 100) / this.Students.Count) > 40) &&
+                    (((this.Students.FindAll(x => x.AverageGrade >= 80).Count * 100) / this.Students.Count) <= 60))
+            {
+                return 'C';
+            }
+            else if ((((this.Students.FindAll(x => x.AverageGrade >= 80).Count * 100) / this.Students.Count) > 60) &&
+                    (((this.Students.FindAll(x => x.AverageGrade >= 80).Count * 100) / this.Students.Count) <= 80))
+            {
+                return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
